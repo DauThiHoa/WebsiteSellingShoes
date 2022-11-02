c3648139017c433aa39accb115481924330d27e7
DauThiHoa <91518695+DauThiHoa@users.noreply.github.com>
2022-11-02 12:13:15 +0700

    Update Price Quantity
    Thay doi gia san pham bang thay doi so luong

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,8 +6,8 @@ import {LoginService} from "../services/login.service";
 import {HttpService} from "../Shared/http.service";
 
 ////////////////////////////////////////////////// LOI GUI MAIL ////////////////////////////////////////////////////////
-// @ts-ignore
-import nodemailer from 'nodemailer';
+// // @ts-ignore
+// import nodemailer from 'nodemailer';
 
 @Component({
   selector: "app-login",
@@ -103,28 +103,28 @@ export class LoginComponent implements OnInit {
     //     this.buttionText = "Submit";
     //   }
     // );
-    let mailTransporter = nodemailer.createTransport({
-      service: "gmail",
-      auth:{
-        user: "daudiep2003@gmail.com",
-        pass: "kxiunzutpxcvbbhf"
-      }
-    })
-
-    let details = {
-      from: "daudiep2003@gmail.com",
-      to : "daudiep2003@gmail.com",
-      subject: "testing our nodemailer",
-      text: "testing out first sender"
-    }
-
-    mailTransporter.sendMail(details, (err : any) =>{
-      if (err){
-        console.log("it has an error" , err);
-      }else {
-        console.log("email has send !");
-      }
-    })
+    // let mailTransporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth:{
+    //     user: "daudiep2003@gmail.com",
+    //     pass: "kxiunzutpxcvbbhf"
+    //   }
+    // })
+    //
+    // let details = {
+    //   from: "daudiep2003@gmail.com",
+    //   to : "daudiep2003@gmail.com",
+    //   subject: "testing our nodemailer",
+    //   text: "testing out first sender"
+    // }
+    //
+    // mailTransporter.sendMail(details, (err : any) =>{
+    //   if (err){
+    //     console.log("it has an error" , err);
+    //   }else {
+    //     console.log("email has send !");
+    //   }
+    // })
 
   }
 
